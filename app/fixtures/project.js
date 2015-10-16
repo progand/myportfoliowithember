30c914c262008c80baf12e1f9d05645fdac3f9b2
progand <lach@ukr.net>
2015-10-16 09:02:52 +0300

    [*] technologies moved to project

--- app/fixtures/project.js
+++ app/fixtures/project.js
@@ -10,8 +10,8 @@ export default [
         description: `Roomlr is a worldwide search engine for every type of holiday home or apartment. Whatever you may wish to rent – chalet, houseboat, apartment or entire house – you'll find it here.`,
         image: 'https://www.roomlr.com/img/logo_roomlr.svg',
         technologies: [
-            'jquery',
-            'backbone'
+            'jQuery',
+            'Backbone'
         ]
 
     }, {
@@ -21,7 +21,7 @@ export default [
         description: `Online service that helps systematize and organize information (text, images, audio, video, etc..), share with your friends.`,
         image: 'https://www.livarava.com/static/livarava/img/projects/livarava-logo-black-200x200.png',
         technologies: [
-            'jquery'
+            'jQuery'
         ]
     }
 ];
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 import PROJECTS from '../fixtures/project';
-import TECHNOLOGIES from '../fixtures/technology';
 
 export default Ember.Route.extend({
     model() {
         return {
             projects: PROJECTS,
-            technologies: TECHNOLOGIES
+            technologies: PROJECTS.reduce((techs, project) => {
+                if(project.technologies){
+                    return techs.concat(project.technologies);
+                }
+            }, []).uniq()
         };
     }
 });
