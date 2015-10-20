f8bc0cbe71d97451fcc755bf6c81faf2b2be6157
progand <lach@ukr.net>
2015-10-20 19:10:46 +0300

    [*] added animations

---
+++ app/transitions.js
@@ -0,0 +1,19 @@
+import Ember from "ember";
+
+export default function () {
+    this.transition(
+        this.fromRoute('index'),
+        this.use('toLeft'),
+        this.reverse('toRight')
+    );
+    this.transition(
+        this.fromRoute('technology'),
+        this.toRoute('project'),
+        this.use('toLeft')
+    );
+    this.transition(
+        this.fromRoute('project'),
+        this.toRoute('technology'),
+        this.use('toLeft')
+    );
+};
--- config/environment.js
+++ config/environment.js
@@ -10,6 +10,8 @@ module.exports = function (environment) {
             FEATURES: {
                 // Here you can enable experimental features on an ember canary build
                 // e.g. 'with-controller': true
+                'query-param': true,
+                'ember-routing-will-change-hooks': true
             }
         },
 
