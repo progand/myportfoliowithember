d5ce4d6648ad349482640a63589465446e74d73f
progand <lach@ukr.net>
2015-10-31 10:06:40 +0200

    [*]  project-list component and filtering

---
+++ app/components/project-list.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    filter: "",
+    visibleProjects: Ember.computed('filter', 'projects', function () {
+        return this.get('projects').filter(project => {
+            return project.name.toLowerCase().indexOf(this.get('filter').toLowerCase()) !== -1;
+        });
+    })
+});
