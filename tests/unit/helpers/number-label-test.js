1862ce377d4b662e8492babd799dbfd2991bde27
progand <lach@ukr.net>
2015-12-29 11:02:33 +0200

    [*]  added mezhynska project

---
+++ tests/unit/helpers/number-label-test.js
@@ -0,0 +1,10 @@
+import { numberLabel } from '../../../helpers/number-label';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | number label');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = numberLabel(42);
+  assert.ok(result);
+});
