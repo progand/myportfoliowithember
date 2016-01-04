966ac44d8e35d9b956eda6de2687f43b883653a9
progand <lach@ukr.net>
2016-01-04 13:08:23 +0200

    [*]  fixed project list

--- app/components/project-list.js
+++ app/components/project-list.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    filter: "",
-    visibleProjects: Ember.computed('filter', 'projects', function () {
-        return this.get('projects').filter(project => {
-            return project.name.toLowerCase().indexOf(this.get('filter').toLowerCase()) !== -1;
-        });
-    })
+  filter: "",
+  visibleProjects: Ember.computed('filter', 'projects', function () {
+    return (this.get('projects')) ? this.get('projects').filter(project => {
+      return project.name.toLowerCase().indexOf(this.get('filter').toLowerCase()) !== -1;
+    }) : [];
+  })
 });
