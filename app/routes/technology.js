22ad0b23e5513218dc0fa75c013a3ed6e59fcea7
progand <lach@ukr.net>
2016-01-04 11:47:01 +0200

    [*]  fixed technology data

--- app/routes/technology.js
+++ app/routes/technology.js
@@ -1,18 +1,10 @@
 import Ember from 'ember';
-import {PROJECTS, TECHNOLOGIES} from '../fixtures/project';
+import {TECHNOLOGIES} from '../fixtures/project';
 
 export default Ember.Route.extend({
   model(params) {
     return _.chain(TECHNOLOGIES)
       .findWhere({id: params.technology_id})
-      /*.cloneDeep()
-      .assign({
-        projects: PROJECTS.filter((project) => {
-          if (project.technologies) {
-            return project.technologies.contains(params.technology_id);
-          }
-        })
-      })*/
       .value();
   }
 });
